8f9cfd12faf2c10ba6432a402cc4cbe050d32dd1
Raj868945 <rcodex0@gmail.com>
2025-12-19 22:22:20 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/api/unban", (req, res) => {
+  res.json({
+    to: "support@whatsapp.com",
+    subject: "WhatsApp Account Unban Request",
+    body:
+`Hello WhatsApp Support Team,
+
+My WhatsApp number was banned by mistake.
+I have not violated any terms and conditions.
+
+Please review my account and unban it.
+
+Thank you`
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("CK-ERROR UNBAN running"));
