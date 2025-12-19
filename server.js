a55a4acdc106b82db40aec7ec485060a4d780464
Raj868945 <rcodex0@gmail.com>
2025-12-19 22:38:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = express();
 
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
 app.get("/api/unban", (req, res) => {
   res.json({
     to: "support@whatsapp.com",
